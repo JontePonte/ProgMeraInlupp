3e9ec51111df0b049d3337cc4304637cdb964472
JontePonte <john_nordstrand@hotmail.com>
2023-02-16 13:56:09 +0100

    Loaded cvs-data to main.py file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
- 
+"Example 1"
+
+import pandas as pd
+
+factbook = pd.read_csv('cia_factbook.csv', sep=';')
+cities = pd.read_csv('worldcities.csv', sep=';')
+pubind = pd.read_csv('worldpubind.csv', sep=';')
+
+print(pubind)
