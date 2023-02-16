1689890a222bc238c61cf31b1dd7221d1b9288fb
JontePonte <john_nordstrand@hotmail.com>
2023-02-16 14:06:12 +0100

    Set the correct index column and removed unnecessary index columns

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 import pandas as pd
 
-factbook = pd.read_csv('cia_factbook.csv', sep=';')
+factbook = pd.read_csv('cia_factbook.csv', sep=';', index_col='Index')
+factbook = factbook.drop("Unnamed: 0", axis=1) 
+# I set index_col='Index' and drop the unnamed because the dataset contain 3 index columns otherwise
+
 cities = pd.read_csv('worldcities.csv', sep=';')
 pubind = pd.read_csv('worldpubind.csv', sep=';')
 
-print(pubind)
+print(factbook)
