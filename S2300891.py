bc8393891a3e540f3da9f8c0891a03fe832c74e7
JontePonte <john_nordstrand@hotmail.com>
2023-02-22 09:12:31 +0100

    New comments and improvements to assignment 2

--- S2300891.py
+++ S2300891.py
@@ -10,7 +10,7 @@ Assignment 1 is always called as it's needed for the other ones.
 
 P.S.
 I try to do all my programming in english. In part because I really need to practice my 
-english writing but mostly because my editor has a english spell-checker :)
+english writing but also because I downloaded a english spellchecker to vscode :)
 """
 import random
 
@@ -24,13 +24,13 @@ import matplotlib.pyplot as plt
 def assignment_1():
     """ Load all data and clean it up, then return all three as Dataframe objects """
 
-    # I set index_col='Index' to avoid an unnecessary index row
+    # I set index_col='Index' to avoid one unnecessary index row
     # The drop unnamed: 0 is done to avoid a second unnecessary index row
     fact_book = pd.read_csv('cia_factbook.csv', sep=';', index_col='Index')
     fact_book = fact_book.drop("Unnamed: 0", axis=1) 
 
     # I thought about using the city id as index but I didn't because
-    # it's difficult to refer to. (=a vanilla read_csv here)
+    # it's difficult to refer to. (= a vanilla read_csv here)
     world_cities = pd.read_csv('worldcities.csv', sep=';')
 
     # I remove the columns "Indicator Name" and "Indicator Code" because
@@ -38,6 +38,7 @@ def assignment_1():
     world_pub_ind = pd.read_csv('worldpubind.csv', sep=';')
     world_pub_ind = world_pub_ind.drop(columns=["Indicator Name", "Indicator Code"])
 
+    # The correct names are set at the bottom of this file
     return fact_book, world_cities, world_pub_ind
 
 
@@ -47,24 +48,26 @@ def assignment_1():
 def assignment_2_plot(df_output, main_title):
     """ The plot function for assignment 2 """
     # I learned to do really fancy plots in this assignment
-
-    # First a really really fancy list comprehension that create random colors
+    # First a really really fancy list comprehension that create random colors for the bars
     colors = ["#{:06x}".format(random.randint(0, 0xFFFFFF)) for _ in range(df_output.shape[0])]
 
     # Plot the relevant results
-    df_output.plot(x='country',
-                   y='density',
-                   kind='bar',          # Bar diagram
-                   color=colors,        # Add the fancy color
-                   legend=False,        # Remove unnecessary legend
-                   figsize=(10,6))      # Make it bigger
+    axis = df_output.plot(x='country',
+                        y='density',
+                        kind='bar',          # Bar diagram
+                        color=colors,        # Add the fancy color
+                        legend=False,        # Remove unnecessary legend
+                        figsize=(10,6))      # Make it bigger
 
+    # Fix rotation and size of country names and numbers
+    axis.tick_params('x', labelrotation=60, labelsize=8)
+    axis.tick_params('y', labelrotation=0, labelsize=8)
+    
     # Set all labels and print the diagram
     plt.xlabel('Country')
     plt.ylabel('[People/km^2]')
-    plt.title(main_title)
+    plt.title(main_title)                           # Main title changes with user input
     plt.subplots_adjust(bottom=0.25)                # Put window in middle of screen
-    plt.xticks(rotation=45)                         # Rotate the names on the x-axis
     plt.show()
 
 
@@ -72,25 +75,25 @@ def assignment_2(_df_cia_factbook):
     """ 
     My solution to assignment 2. The function is called at the bottom of this file.
     I allowed more than 10 countries to be plotted. I can change it if you like
-    but I think it's nice to also be able to see many of them at once.
+    but I think it's interesting to also be able to see many of them at once.
     """
 
-    # Create the pandas series density and add it to df_cia_factbook dataframe
+    # Create the pandas series "density" and add it to _df_cia_factbook dataframe
     density = _df_cia_factbook['population'] / _df_cia_factbook['area']
     _df_cia_factbook['density'] = density
 
-    # I save the original index before dropna and sorting for a fancy plot later on
+    # I save the original index before dropna and sorting for a fancy extra plot later on
     original_index = _df_cia_factbook.index
 
     # This changes the inf for Vatican City to NaN. This enables dropna for Vatican City and
-    # all other countries with NaN density
+    # all other countries with NaN density (I think it's only Vatican for this data set)
     _df_cia_factbook = _df_cia_factbook.replace({np.inf:np.nan})
     _df_cia_factbook = _df_cia_factbook.dropna(subset=['density'])
 
     # Sort the dataframe and put the highest density first
     fb_sorted = _df_cia_factbook.sort_values(by='density', ascending=False)
 
-    # Add a sorted index to enable fancy plot later on
+    # Add a sorted index to enable the fancy plot later on
     fb_sorted = fb_sorted.set_index(original_index[0:fb_sorted.shape[0]])
 
     print('')
@@ -107,7 +110,7 @@ def assignment_2(_df_cia_factbook):
     # Just a small check to enable user to fix the input
     is_input_bad = True
     while is_input_bad:
-        is_input_bad = False                    # input is not bad until proven otherwise
+        is_input_bad = False # input is not bad until proven otherwise
         user_input = str(input('     Input:'))
 
         # sort the user input based on what type it is
@@ -128,20 +131,26 @@ def assignment_2(_df_cia_factbook):
             assignment_2_plot(df_low, main_title)
 
         # Check if user input match any of the countries
-        # (I think this breaks down if the user input is for example 'a')
+        # (I think this breaks down if the user input is for example 'a' it could be done
+        # better by creating a set of all countries like in later assignments)
         elif fb_sorted['country'].str.contains(user_input).any():
             # I do a fancy thing here,
             # I collect the 6 closest countries i a dataframe instead of just printing it
             row_index = fb_sorted.loc[fb_sorted['country'] == user_input].index[0]
 
+            # The middle index is set so that the 3 highest and lowest countries can
+            # be plotted (the 7 highest or lowest densities are plotted then)
             row_index = 4 if row_index < 4 else row_index
             row_index = fb_sorted.shape[0]-3 if row_index > fb_sorted.shape[0]-3 else row_index
 
+            # Create a dataframe with the selected country and the 6 closest
             df_country = fb_sorted.iloc[row_index-4 : row_index+3]
 
             # Send the fancy dataframe and title to plotting function
-            # fb_sorted.iloc[row_index]['density']:.1f extracts population density with one decima
+            # fb_sorted.iloc[row_index]['density']:.1f extracts population density with one decimal
             main_title = f"The population density of {user_input} is {fb_sorted.iloc[row_index]['density']:.1f} people per km^2"
+            
+            # Send to plot
             assignment_2_plot(df_country, main_title)
 
         else:
@@ -149,7 +158,6 @@ def assignment_2(_df_cia_factbook):
             print('')
             print('')
             is_input_bad = True # We were proven otherwise, go back in the loop
-
 
 
 ####################################### 3 start #######################################
@@ -668,8 +676,8 @@ df_cia_factbook, df_worldcities, df_worldpubind = assignment_1() # assignment_1 
 The functions for the assignments are called here.
 You should probably only have 1 uncommented.
 """
-# assignment_2(df_cia_factbook)
+assignment_2(df_cia_factbook)
 # assignment_3(df_cia_factbook)
 # assignment_4a(df_worldpubind)
 # assignment_4b(df_worldpubind)
-assignment_5(df_worldcities)
+# assignment_5(df_worldcities)
